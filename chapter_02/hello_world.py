a29f36285a0f08a3982906fc0891463adff1344c
Kuluny <lokilek2446@gmail.com>
2022-06-03 03:02:36 -0400

    Variables and simple data types

---
+++ chapter_02/hello_world.py
@@ -0,0 +1,16 @@
+"""
+_Variables_: Assigns a value to something. 
+Naming & using variables:
+    1. Can contain only letters, numbers, & underscorers(_). Ex. message_1 but not 1_message.
+    2. Spaces are not allowed. Ex greeting_message but not greeting message.
+    3. Avoid using Python keyword
+    4. Should be short but descriptive. Ex name_lenght.
+"""
+
+#Add a variable called message  
+message = 'This is a Python Crash Course.'
+print(message)
+
+#Add a second variable also called message
+message = 'This crash course will bring you up to speed with Python.'
+print(message)
