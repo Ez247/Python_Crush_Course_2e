4f3ae239dbc4886cf302421a5570c4c972e1635c
Kuluny <lokilek2446@gmail.com>
2022-06-03 11:25:33 -0400

    Variable and simple data type

---
+++ chapter_02/name.py
@@ -0,0 +1,10 @@
+"""_strings_: A series of characters.
+    NB: Anything inside quotes(double or single) is considered a string. 
+"""
+#Example
+"This is an example of a string."
+"This is also considered a string."
+
+#Changing Case in a String with Methods.
+name = 'joe doe'
+print(name.title())
