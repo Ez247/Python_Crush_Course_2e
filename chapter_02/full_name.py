0fb0c0636b9e71feba320ce3ba6e5e361d99c23e
Kuluny <lokilek2446@gmail.com>
2022-06-03 19:11:16 -0400

    Add f-string example

---
+++ chapter_02/full_name.py
@@ -0,0 +1,9 @@
+"""_f strings_: f stands for format, & you can do a lot with f strings. 
+        1. You can use f-strings to compose complete message using the below statements.
+        2. f-strings were first introduced in Python 3.6
+"""
+first_name = 'kevin'
+last_name = 'lovelace'
+full_name = f"{first_name} {last_name}"
+print('Hello, ' + full_name)
+print(f"Hello, {full_name.title()}!") #notice the difference between the 2 print statements
